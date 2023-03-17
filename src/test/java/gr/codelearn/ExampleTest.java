3f6ca7e305615b39219629a8c37964d5bc5a18bf
Ioannis <jondan97@gmail.com>
2023-03-17 17:48:49 +0200

    added testing through maven

---
+++ src/test/java/gr/codelearn/ExampleTest.java
@@ -0,0 +1,27 @@
+package gr.codelearn;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ExampleTest {
+    @Test
+    void test1(){
+        System.out.println("Performing Test 1");
+    }
+
+    @Test
+    void test2(){
+        System.out.println("Performing Test 2");
+    }
+
+    @Test
+    void test3(){
+        System.out.println("Performing Test 3");
+    }
+
+    @Test
+    void test4(){
+        System.out.println("Performing Test 4");
+    }
+}
